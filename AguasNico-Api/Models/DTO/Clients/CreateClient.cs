55dbe4fa9cf779ca01f1a2c0df09710595a1b91c
Martin Sola <martinrsola55@gmail.com>
2026-06-17 23:46:31 -0300

    Fix clients

--- AguasNico-Api/Models/DTO/Clients/CreateClient.cs
+++ AguasNico-Api/Models/DTO/Clients/CreateClient.cs
@@ -13,10 +13,10 @@ public class CreateClientRequest
     public string DealerId { get; set; }
     public bool HasInvoice { get; set; }
     public bool OnlyAbonos { get; set; }
-    public InvoiceType InvoiceType { get; set; }
-    public TaxCondition TaxCondition { get; set; }
+    public InvoiceType? InvoiceType { get; set; }
+    public TaxCondition? TaxCondition { get; set; }
     public string CUIT { get; set; }
-    public Day DeliveryDay { get; set; }
+    public Day? DeliveryDay { get; set; }
     public List<ClientProductRequestItem> Products { get; set; } = [];
     public List<long> AbonoIds { get; set; } = [];
 }
--- AguasNico-Api/Models/DTO/Clients/UpdateInvoiceData.cs
+++ AguasNico-Api/Models/DTO/Clients/UpdateInvoiceData.cs
@@ -4,8 +4,8 @@ namespace AguasNico_Api.Models.DTO.Clients;
 public class UpdateInvoiceDataRequest
 {
     public long Id { get; set; }
-    public InvoiceType InvoiceType { get; set; }
-    public TaxCondition TaxCondition { get; set; }
+    public InvoiceType? InvoiceType { get; set; }
+    public TaxCondition? TaxCondition { get; set; }
     public string CUIT { get; set; }
 }
 
