c718ad5b648a54a2a50110503f065914d0a7efa8
Martín Sola <101649619+MartinSola55@users.noreply.github.com>
2026-07-07 18:33:09 -0300

    Fix permissions and GetDashboard endpoint (#4)
    * Fix permissions and GetDashboard endpoint
    
    * Remove unused data

--- AguasNico-Api/Models/DTO/Home/GetDashboard.cs
+++ AguasNico-Api/Models/DTO/Home/GetDashboard.cs
@@ -1,21 +1,14 @@
-﻿using AguasNico_Api.Models.DTO.Common;
-using AguasNico_Api.Models.DTO.Expenses;
+﻿using AguasNico_Api.Models.Constants;
 using AguasNico_Api.Models.DTO.Routes;
-
 
 namespace AguasNico_Api.Models.DTO.Home;
 
-public class GetDashboardResponse
+public class GetDashboardRequest
 {
-    public string Role { get; set; }
-    public List<SoldProductsItem> SoldProducts { get; set; } = [];
-    public List<ExpenseItem> Expenses { get; set; } = [];
-    public List<PaymentAmountItem> Payments { get; set; } = [];
-    public decimal TotalSold { get; set; }
-    public List<TransferItem> Transfers { get; set; } = [];
-    public decimal Dispensers { get; set; }
-    public List<RouteItem> DealerRoutes { get; set; } = [];
-    public List<DealerItem> Dealers { get; set; } = [];
+    public Day Day { get; set; }
 }
 
-
+public class GetDashboardResponse
+{
+    public List<RouteItem> DealerRoutes { get; set; } = [];
+}
